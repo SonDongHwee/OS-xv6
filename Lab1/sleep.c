56989f9e67177c54977c40d9884f52a8a38938a3
donghwee0402@gmail.com <donghwee0402@gmail.com>
2023-01-05 17:05:35 +0900

    First commit

---
+++ Lab1/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc,char** argv){
+    if(argc != 2){
+        fprintf(2, "usage: sleep clocktick ...\n");
+        exit(1);    
+    }
+    
+    if(sleep(atoi(argv[1])) < 0){
+        fprintf(2, "sleep error\n");
+    }   
+    exit(0);
+}
